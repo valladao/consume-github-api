d7d74a8db82d6c4ebd8b2a37b5db351c5b62df86
valladao <valladao@gmail.com>
2021-06-15 07:46:33 -0300

    Create the form

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!")
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form")
+  form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const username = document.querySelector("input").value
+    alert(username)
+  })
+})
--- dist/app.js
+++ dist/app.js
@@ -118,7 +118,14 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"app.js":[function(require,module,exports) {
-console.log("Hello, World!");
+document.addEventListener("DOMContentLoaded", function () {
+  var form = document.querySelector("form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var username = document.querySelector("input").value;
+    alert(username);
+  });
+});
 },{}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +154,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49278" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55247" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
